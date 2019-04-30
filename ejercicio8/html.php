886cab7b9e9ea095a291025a7a278f10ac6b041a
LAPTOP-NPUG685Q\HP <kelly.soto@epn.edu.ec>
2019-04-30 09:33:53 -0500

    ejercicio8

---
+++ ejercicio8/html.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<link href="css.css" rel="stylesheet" type="text/css" />
+<html>
+<head>
+	<title>Dias del año</title>
+</head>
+<body>
+
+
+<?php include ('php.php') ?>
+
+</body>
+</html>
